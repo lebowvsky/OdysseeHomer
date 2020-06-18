72224d89ad8c6e9a6d4247c4ce812ebae4f1b208
Brice Le Gallo <lebowvsky@gmail.com>
2020-06-18 23:30:09 +0200

    030 Done

---
+++ back/app.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+const authRouter = require('./routes/auth/auth')
+
+
+app.use('/auth', authRouter);
+
+app.get("/", (req,res) => {
+  res.send("youhou");
+})
+/// dans le cas d'une route non trouvée, je retourne le code 404 'Not Found'
+app.use(function(req, res, next) {
+  var  err  =  new  Error('Not Found');
+  err.status  =  404;
+  next(err);
+});
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended:  false }));
+app.use(bodyParser.json());
+app.use(express.static(__dirname  +  '/public'));
+
+const server = app.listen (process.env.PORT || 4545, function(){
+  console.log('Listening on port '  +  server.address().port);
+})
